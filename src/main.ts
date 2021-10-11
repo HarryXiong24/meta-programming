d218b79c36010d4a1d2fc552a7df977a211f3bf9
harryxiong <harryxiong24@gmail.com>
2021-10-11 10:59:57 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import router from "./router";
+import store from "./store";
+
+const app = createApp(App);
+
+app.use(Antd);
+app.use(store);
+app.use(router);
+app.mount("#app");
---
+++ src/views/Demo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="demo">
+    <h1>This is an demo page</h1>
+    <a-row justify="center">
+      <a-col :span="20">
+        <a-table :dataSource="dataSource" :columns="columns" />
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      dataSource: [
+        {
+          key: "1",
+          name: "胡彦斌",
+          age: 32,
+          address: "西湖区湖底公园1号",
+        },
+        {
+          key: "2",
+          name: "胡彦祖",
+          age: 42,
+          address: "西湖区湖底公园1号",
+        },
+      ],
+      columns: [
+        {
+          title: "姓名",
+          dataIndex: "name",
+          key: "name",
+        },
+        {
+          title: "年龄",
+          dataIndex: "age",
+          key: "age",
+        },
+        {
+          title: "住址",
+          dataIndex: "address",
+          key: "address",
+        },
+      ],
+    };
+  },
+});
+</script>
